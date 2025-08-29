bc3567e08c9fa6b3c4268f04c2b6f2dc497ada40
hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>
2025-08-29 06:02:33 +0000

    root: Initial project
    This is the initial commit for the project. It contains various project
    files meant to help me get new Rust projects off the ground. This
    includes configs for rustfmt and clippy, various markdown documents,
    licensing, CI, and a local config for cargo.
    
    Signed-off-by: hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: Copyright © 2025 hashcatHitman
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+//! reading makes you smart
+
+/// This isn't a real project.
+///
+/// ```rust
+/// println!("{}", rust_project_template::read_a_book())
+/// ```
+pub const fn read_a_book() -> &'static str {
+    "you are so smart!!"
+}
---
+++ src/main.rs
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: Copyright © 2025 hashcatHitman
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+//! This isn't a real project.
+
+/// This isn't a real project.
+///
+/// ```rust
+/// println!("hello")
+/// ```
+fn main() {
+    println!("{}", rust_project_template::read_a_book());
+}
