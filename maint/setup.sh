de3042300f47249800a6d0a7a975123c03072898
hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>
2026-09-21 05:41:57 +0000

    ops: Move the shebang in `setup.sh`
    Signed-off-by: hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>

--- maint/setup.sh
+++ maint/setup.sh
@@ -1,8 +1,8 @@
+#!/usr/bin/env bash
+
 # SPDX-FileCopyrightText: Copyright © 2025 hashcatHitman
 #
 # SPDX-License-Identifier: Apache-2.0 OR MIT
-
-#!/usr/bin/env bash
 
 # Create .cargo directory if it does not exist and copy the local config to it.
 mkdir -p ./.cargo && cp ./maint/suggested_config.toml ./.cargo/config.toml;
